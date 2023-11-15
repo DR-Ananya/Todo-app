0b41033a69067aa82890060be90c90f9624cba82
DRAnanya <ananya@jalantechnologies.com>
2023-11-15 09:00:32 +0530

    adding comments

--- src/components/addtodo.tsx
+++ src/components/addtodo.tsx
@@ -1,22 +1,39 @@
-import { FormEvent, useState } from "react"
+// Import FormEvent and useState from React. Import useTodos from the "todos" store.
+import { FormEvent, useState } from "react";
 import { useTodos } from "../store/todos";
 
+// Define the AddToDo functional component.
 const AddToDo = () => {
-    const[todo, setTodo] = useState("");
-    const {handleAddToDo} = useTodos();
- 
-    const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        handleAddToDo(todo)
-        setTodo("")
-    }
+  // Declare state variables todo and setTodo using the useState hook.
+  const [todo, setTodo] = useState("");
 
+  // Destructure the handleAddToDo function from the useTodos custom hook.
+  const { handleAddToDo } = useTodos();
+
+  // Define handleFormSubmit function to handle form submission.
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission behavior.
+
+    // Call the handleAddToDo function from the useTodos hook with the current todo value.
+    handleAddToDo(todo);
+
+    // Clear the todo input field after submitting the form.
+    setTodo("");
+  };
+
+  // Return a form element with an input field and a button.
+  // Handle input changes and form submission.
   return (
     <form onSubmit={handleFormSubmit}>
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button type="submit">Add</button>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
-  )
-}
+  );
+};
 
-export default AddToDo
+// Export the AddToDo component as the default export.
+export default AddToDo;
